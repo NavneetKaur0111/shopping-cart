2ea99cbbf13f2bd3a17e945116350e1437f6d100
navneet <navneet3265@gmail.com>
2020-12-03 14:42:38 -0600

    modify, concatenate and create sourcemap for js files

--- src/js/courses.js
+++ src/js/courses.js
@@ -1,6 +1,13 @@
-
 class Course {
-  constructor(id, price, instructor, availableSpaces, title, description, image) {
+  constructor(
+    id,
+    price,
+    instructor,
+    availableSpaces,
+    title,
+    description,
+    image
+  ) {
     this.id = id;
     this.price = price;
     this.instructor = instructor;
@@ -11,20 +18,19 @@ class Course {
   }
 }
 
-const cartEle = document.querySelector('.popover');
-const coursesEle = document.querySelector('.courses');
+const cartEle = document.querySelector(".popover");
+const coursesEle = document.querySelector(".courses");
 
-cartEle.addEventListener('click', function() {
-  document.querySelector('.cart-inner').classList.toggle('hide');
+cartEle.addEventListener("click", function () {
+  document.querySelector(".cart-inner").classList.toggle("hide");
 });
-
 
 const courses = [
   new Course(
     "SD100",
     154.99,
     "Chris MacDonald",
-    15, 
+    15,
     "Introduction to Web Development",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     "html_and_css.jpg"
@@ -33,7 +39,7 @@ const courses = [
     "SD110",
     223.01,
     "Chris MacDonald",
-    3, 
+    3,
     "JavaScript Basics",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     "js-basics.png"
@@ -42,16 +48,16 @@ const courses = [
     "SD120",
     99.99,
     "Chris MacDonald",
-    10, 
+    10,
     "Object Oriented JavaScript",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     "advanced-javascript.png"
-  ),  
+  ),
   new Course(
     "SD145",
     49.99,
     "Chris MacDonald",
-    30, 
+    30,
     "JavaScript Testing",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     "testing.png"
@@ -60,7 +66,7 @@ const courses = [
     "SD260",
     149.99,
     "Chris MacDonald",
-    5, 
+    5,
     "Introduction to React",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     "react.jpg"
@@ -69,16 +75,17 @@ const courses = [
     "SD130",
     149.99,
     "Chris MacDonald",
-    15, 
+    15,
     "Tools and Automation",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     "tools.png"
   ),
 ];
 
-
-courses.forEach(function(course) {
-  coursesEle.insertAdjacentHTML('afterbegin', `
+courses.forEach(function (course) {
+  coursesEle.insertAdjacentHTML(
+    "afterbegin",
+    `
     <li data-course-id="${course.id}">
       <img src="images/${course.image}">
       <div class="info">
@@ -97,5 +104,6 @@ courses.forEach(function(course) {
         <button id="add-to-cart">Add To Cart</button>
         <div id="spaces-remaining"><span>${course.availableSpaces}</span> spaces remaining</div>
       </div>
-    </li>`);
+    </li>`
+  );
 });
